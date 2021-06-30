dfc565ca77969df3649fd3c4a3b5f0475332e5a4
Edson <edsonst1@gmail.com>
2021-06-30 09:17:37 -0300

    Changes related to decoding morse code and use of a iambic key device

--- Iambic_Keyer.ino
+++ Iambic_Keyer.ino
@@ -1,65 +1,101 @@
 // Simple Iambic Keyer v1.00
 // by Ernest PA3HCM
+// Changed by PU2TNO
 
-#include <Adafruit_RGBLCDShield.h>
-
-/*
 #define P_DOT    2   // Connects to the dot lever of the paddle
 #define P_DASH   3   // Connects to the dash lever of the paddle
-#define P_AUDIO 12   // Audio output
-#define P_CW    13   // Output of the keyer, connect to your radio
-#define P_SPEED A0   // Attached to center pin of potmeter, allows you
-                     // to set the keying speed.
-*/
 
-
+#include <Adafruit_RGBLCDShield.h>
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
-int speed = 160;
+int speed = 60;
+unsigned long t0,t1,t2;
+boolean flag = false;
+char* lookupString = ".EISH54V.3UF....2ARL.....WP..J.1TNDB6.X..KC..Y..MGZ7.Q..O.8..90.";
+byte currentDecoderIndex = 0;
+byte currentDashJump = 64;
+char currentAssumedChar='\0';
+byte curPos = 0;
 
 // Initializing the Arduino
 void setup()
 {
   lcd.begin(16,2);
+  lcd.cursor();
   lcd.clear();
   lcd.print (1200/speed);
   lcd.print(" WPM");
-/*  pinMode(P_DOT, INPUT);
-  pinMode(P_DASH, INPUT); 
-  pinMode(P_AUDIO, OUTPUT);
-  pinMode(P_CW, OUTPUT);
-  digitalWrite(P_CW, LOW); */     // Start with key up
+  lcd.setCursor(0,1);
+  pinMode(P_DOT,INPUT_PULLUP);
+  pinMode(P_DASH,INPUT_PULLUP );
 }
 
 // Main routine
 void loop()
 {
-  //speed = analogRead(P_SPEED)/2; // Read the keying speed from potmeter
-  if (lcd.readButtons() & BUTTON_LEFT)
-  //if(!digitalRead(P_DOT))        // If the dot lever is presssed..
+  if ((lcd.readButtons() & BUTTON_LEFT) || (!digitalRead(P_DOT))) // If the dot lever is presssed..     
   {
-    keyAndBeep(speed);           // ... send a dot at the given speed
-    delay(speed);                //     and wait before sending next
+    keyAndBeep(speed);            // ... send a dot at the given speed
+    //t0 = millis();
+    currentAssumedChar = lookup('.');
+    //t1 = millis()-t0;
+    //delay(speed-t1);                //     and wait before sending next
+    delay(speed);
+    t2 = millis();
+    flag = true;
   }
-  if (lcd.readButtons() & BUTTON_RIGHT)
-  //if(!digitalRead(P_DASH))       // If the dash lever is pressed...
+  if ((lcd.readButtons() & BUTTON_RIGHT) || (!digitalRead(P_DASH))) // If the dash lever is pressed...      
   {
     keyAndBeep(speed*3);         // ... send a dash at the given speed
-    delay(speed);                //     and wait before sending next
+    //t0 = millis();
+    //lcd.print("_");
+    currentAssumedChar = lookup('-');
+    //t1 = millis()-t0;
+    //delay(speed-t1);                //     and wait before sending next
+    delay(speed);
+    t2 = millis();
+    flag = true;
   }
+  if ((millis()-t2 > speed) & flag)
+  { 
+    //lcd.print(" ");
+   // lcd.clear();
+    t0 = millis();
+    lcd.print(currentAssumedChar);
+    curPos++;
+    if (curPos == 16)
+    {  
+      lcd.setCursor(0,1);
+      curPos = 0;      
+    }  
+    lookup('\0');
+    flag = false;
+    t1 = millis()-t0;
+    delay (3*speed-t1);
+  }       
 }
-
 // Key the transmitter and sound a beep
 void keyAndBeep(int speed)
 {
-  /* digitalWrite(P_CW, HIGH);            // Key down
-  for (int i=0; i < (speed/2); i++)    // Beep loop
-  {
-    digitalWrite(P_AUDIO, HIGH);
-    delay(1);
-    digitalWrite(P_AUDIO, LOW);
-    delay(1);
-  }
-  digitalWrite(P_CW, LOW);   */          // Key up
   tone (8,700,speed);
   delay (speed);
+}
+
+char lookup(char currentMark)
+{
+    currentDashJump = floor(currentDashJump / 2.0f);
+    if (currentMark == '.')
+    {                
+        currentDecoderIndex++;
+    }
+    else if (currentMark == '-')
+    {
+        currentDecoderIndex += currentDashJump;
+    }
+    else if (currentMark == '\0')
+    {
+        currentDecoderIndex = 0;
+        currentDashJump = 64;
+        return '\0';
+    }
+    return lookupString[currentDecoderIndex];
 }
